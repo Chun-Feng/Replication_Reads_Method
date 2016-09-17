2dabb016871c18af133da464c153818e4052b832
Chun-Feng, Chen <chunfeng8204@gmail.com>
2016-09-17 23:39:27 +0800

    Add files via upload

---
+++ exec.sh
@@ -0,0 +1,17 @@
+#!/bin/tcsh
+
+source /fpga/xilinx.cshrc
+source /usr/cad/env.cshrc
+
+ncverilog \
+              -y $XILINX/verilog/src/unisims \
+              -y $XILINX/verilog/src/XilinxCoreLib \
+              +incdir+$XILINX/verilog/src \
+              +libext+.v \
+              $XILINX/verilog/src/glbl.v \
+              ./blk_mem_gen_v7_2.v \
+              ./ram_1R1W.v \
+              ./TESTBENCH.v +define+RTL +access+rw +notimingchecks
+
+rm -r INCA_libs
+
